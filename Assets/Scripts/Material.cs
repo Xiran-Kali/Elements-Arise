8ec0862cd812d6d689d78b02817c62c54a6ba37f
Xiran Kali <lucas.silver999@gmail.com>
2024-10-21 15:32:34 +0200

    Ajout ObjectData

--- Assets/Scripts/Material.cs
+++ Assets/Scripts/Material.cs
@@ -61,15 +61,4 @@ public class Material : MonoBehaviour
     }
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/ObjectData.cs
+++ Assets/Scripts/ObjectData.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class ObjectData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private int temperature;
+
+    public int Temperature {  get { return temperature; } set { temperature = value; } }
+
+
+    [SerializeField]
+    private int humidity;
+
+    public int Humidity { get { return humidity; } set { humidity = value; } }
+
+    [SerializeField]
+    private float mass;
+
+
+    public bool electrified;
+
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+
+        Spell spell = other.GetComponent<Spell>();
+
+        if (spell != null)
+        {
+            Debug.Log("truc");
+        }
+
+
+
     }
 }
