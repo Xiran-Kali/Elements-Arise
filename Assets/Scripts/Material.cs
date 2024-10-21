1811b5bf883f0a8066d3cd60fd46a8950cd1997e
Xiran Kali <lucas.silver999@gmail.com>
2024-10-21 14:57:33 +0200

    Ajout des fichiers des scripts

---
+++ Assets/Scripts/Material.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Material : MonoBehaviour
+{
+    protected bool conductivite;
+    protected float mass_multiplier;
+
+    protected virtual void OnFire()
+    {
+
+    }
+
+    protected virtual void OnWater()
+    {
+
+    }
+    protected virtual void OnEarth()
+    {
+
+    }
+    protected virtual void OnWind()
+    {
+
+    }
+    protected virtual void OnSteam()
+    {
+
+    }
+    protected virtual void OnVolcano()
+    {
+
+    }
+    protected virtual void OnThunder()
+    {
+
+    }
+    protected virtual void OnPlant()
+    {
+
+    }
+    protected virtual void OnIce()
+    {
+
+    }
+
+    protected virtual void OnSand()
+    {
+
+    }
+
+    protected virtual void Burning()
+    {
+
+    }
+
+    protected virtual bool Frozen()
+    {
+        return false;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
